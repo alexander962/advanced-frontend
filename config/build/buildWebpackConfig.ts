5f172c1ca48b3f7af325bb1188082f90c126e192
alexander962 <sanek12011996@gmail.com>
2024-09-07 14:26:12 +0300

    improved webpack structure

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,31 @@
+import {BuildOptions} from "./types/config";
+import webpack from "webpack";
+import {buildPlugins} from "./buildPlugins";
+import {buildLoaders} from "./buildLoaders";
+import {buildResolvers} from "./buildResolvers";
+
+export const buildWebpackConfig = (options: BuildOptions): webpack.Configuration => {
+  const {paths, mode} = options;
+
+  return {
+    mode,
+    entry: {
+      RANDOM: paths.entry,
+    },
+    // указываем куда и как будем делать сборку приложения
+    output: {
+      // хеширование файлов
+      filename: "[name].[contenthash].js",
+      path: paths.build,
+      // чтобы подчищать ненужные файлы
+      clean: true,
+    },
+    plugins: buildPlugins(options),
+    module: {
+      // конфигурируем лоэдеры, они обрабатывают файлы, которые выходят за рамки js
+      // png, jpeg, svg, css, scss, ts и другие
+      rules: buildLoaders(),
+    },
+    resolve: buildResolvers(),
+  }
+}
