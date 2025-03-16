1176a90a411c4e19828d778664f7ace8ce2352f7
alexander962 <sanek12011996@gmail.com>
2025-03-16 13:03:58 +0300

    feat: add navbar, button, svg and files loaders

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,19 +1,17 @@
 import React, {  FC } from 'react';
-import { Link } from "react-router-dom";
 import { useTheme } from "app/providers/ThemeProvider";
 import { classNames } from "shared/lib/classNames/classNames";
+import { AppRouter } from "app/providers/router";
+import { Navbar } from "widgets/Navbar";
 import './styles/index.scss';
-import {AppRouter} from "app/providers/router";
 
 export const App: FC = () => {
-  const {theme, toggleTheme} = useTheme()
+  const { theme } = useTheme();
 
   return (
     <div className={classNames('app', {}, [theme])}>
-      <button onClick={toggleTheme}>TOGGLE</button>
-      <Link to={'/'}>Главная</Link>
-      <Link to={'/about'}>О сайте</Link>
-      <AppRouter />
+      <Navbar/>
+      <AppRouter/>
     </div>
   );
 };
