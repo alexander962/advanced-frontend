bc00356a1ca974f82903542b1e48c4a06ad023a2
alexander962 <sanek12011996@gmail.com>
2025-03-19 16:38:12 +0300

    feat: add new parameter eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -55,6 +55,7 @@ module.exports = {
     'no-underscore-dangle': 'off',
     // делаем, чтобы ругалс на отсутствие переводов только внутри jsx
     'i18next/no-literal-string': ['error', { markupOnly: true }],
+    'max-len': ['error', { ignoreComments: true }],
   },
   globals: {
     __IS_DEV__: true,
