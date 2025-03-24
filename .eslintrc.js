027f08a4521f33e2713d4d71d46cc4515b87338f
alexander962 <sanek12011996@gmail.com>
2025-03-24 21:37:20 +0300

    feat: add storybook, theme and style decorators

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,7 @@ module.exports = {
     es2021: true,
     jest: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-    'plugin:i18next/recommended',
-  ],
+  extends: ['plugin:react/recommended', 'airbnb', 'plugin:i18next/recommended', 'plugin:storybook/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
@@ -58,22 +54,22 @@ module.exports = {
       'error',
       {
         markupOnly: true,
-        ignoreAttribute: ['data-testid'],
+        ignoreAttribute: ['data-testid', 'to'],
       },
     ],
     'max-len': ['error', { ignoreComments: true, code: 100 }],
   },
   globals: {
     __IS_DEV__: true,
-    // переопределяем правила для определенных файлов
-    overrides: [
-      {
-        // отключаем переводы для тестовых файлов
-        files: ['**/src/**/*.test.{ts, tsx}'],
-        rules: {
-          'i18next/no-literal-string': 'off'
-        }
-      }
-    ]
   },
+  // переопределяем правила для определенных файлов
+  overrides: [
+    {
+      // отключаем переводы для тестовых файлов
+      files: ['**/src/**/*.test.{ts,tsx}'],
+      rules: {
+        'i18next/no-literal-string': 'off',
+      },
+    },
+  ],
 };
