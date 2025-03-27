87d832804a64294cf49bd961574613ddf05807f4
alexander962 <sanek12011996@gmail.com>
2025-03-27 11:55:18 +0300

    feat: add loki ui tests

---
+++ loki.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  storybookUrl: 'http://localhost:6006',
+  chromeSelector: '#storybook-root .app',
+  renderTimeout: 20000,
+  scenarios: [],
+  configurations: {
+    chromeLaptop: {
+      target: 'chrome.app',
+      width: 1366,
+      height: 768,
+    },
+  },
+  onReady: async ({ page }) => {
+    // Ждём появления какого-то элемента на странице, например, заголовка
+    await page.waitForSelector('h1', { timeout: 15000 });
+  },
+};
