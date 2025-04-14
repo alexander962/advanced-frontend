e92fab16bb96a262b3c0d3e3e146705afea68612
alexander962 <sanek12011996@gmail.com>
2025-04-14 21:34:13 +0500

    fix theme for modal

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -63,7 +63,7 @@ export const Modal: FC<ModalProps> = ({
 
   return (
     <Portal>
-      <div className={classNames(cls.Modal, mods, [className, theme])}>
+      <div className={classNames(cls.Modal, mods, [className, theme, 'app_modal '])}>
         <div className={cls.overlay} onClick={closeHandler}>
           <div className={cls.content} onClick={onContentClick}>{children}</div>
         </div>
