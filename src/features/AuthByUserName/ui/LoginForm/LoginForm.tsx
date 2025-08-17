2b9bc32acfdea360662f67b91ba454e47227ac69
alexander962 <sanek12011996@gmail.com>
2025-08-17 20:50:44 +0300

    add Text component and service loginByUsername

--- src/features/AuthByUserName/ui/LoginForm/LoginForm.tsx
+++ src/features/AuthByUserName/ui/LoginForm/LoginForm.tsx
@@ -4,6 +4,8 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 import { Input } from 'shared/ui/Input/Input';
 import { useDispatch, useSelector } from 'react-redux';
+import { Text, TextTheme } from 'shared/ui/Text/Text';
+import { loginByUsername } from '../../model/service/loginByUsername/loginByUsername';
 import { getLoginState } from '../../model/selectors/getLoginState/getLoginState';
 import { loginActions } from '../../model/slice/loginSlice';
 import cls from './LoginForm.module.scss';
@@ -14,8 +16,11 @@ interface LoginFormProps {
 
 export const LoginForm = memo(({ className }: LoginFormProps) => {
   const { t } = useTranslation();
+  // для работы с экшионами
   const dispatch = useDispatch();
-  const { username, password } = useSelector(getLoginState);
+  const {
+    username, password, error, isLoading,
+  } = useSelector(getLoginState);
 
   // для всех функций, которые мы передаем пропсом, мы используем useCallback
   // чтобы ссылка у нас никогда не менялась
@@ -28,11 +33,13 @@ export const LoginForm = memo(({ className }: LoginFormProps) => {
   }, [dispatch]);
 
   const onLoginClick = useCallback(() => {
-    //
-  }, []);
+    dispatch((loginByUsername({ username, password })));
+  }, [dispatch, password, username]);
 
   return (
     <div className={classNames(cls.LoginForm, {}, [className])}>
+      <Text title={t('Форма авторизации')} />
+      {error && <Text text={error} theme={TextTheme.ERROR} />}
       <Input
         autoFocus
         type="text"
@@ -48,7 +55,12 @@ export const LoginForm = memo(({ className }: LoginFormProps) => {
         onChange={onChangePassword}
         value={password}
       />
-      <Button theme={ButtonTheme.OUTLINE} className={cls.loginBtn} onClick={onLoginClick}>
+      <Button
+        theme={ButtonTheme.OUTLINE}
+        className={cls.loginBtn}
+        onClick={onLoginClick}
+        disabled={isLoading}
+      >
         {t('Войти')}
       </Button>
     </div>
--- src/shared/ui/Button/Button.stories.tsx
+++ src/shared/ui/Button/Button.stories.tsx
@@ -107,3 +107,11 @@ export const SquareSizeXL: Story = {
     size: ButtonSize.XL,
   },
 };
+
+export const Disabled: Story = {
+  args: {
+    children: '>',
+    theme: ButtonTheme.OUTLINE,
+    disabled: true,
+  },
+};
