f70d0720e953ced9c4362f380a9327c4db0a566b
alexander962 <sanek12011996@gmail.com>
2025-08-27 20:15:26 +0300

    add dynamic module loader component

---
+++ src/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader.tsx
@@ -0,0 +1,55 @@
+import { FC, useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useDispatch, useStore } from 'react-redux';
+import { ReduxStoreWithManager, StateSchemaKey } from 'app/providers/StoreProvider/config/StateSchema';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Reducer } from '@reduxjs/toolkit';
+import cls from './DynamicModuleLoader.module.scss';
+
+export type ReducersList = {
+  [name in StateSchemaKey]?: Reducer;
+}
+
+type ReducersListEntry = [StateSchemaKey, Reducer];
+
+interface DynamicModuleLoaderProps {
+  reducers: ReducersList;
+  removeAfterUnmount?: boolean;
+}
+
+export const DynamicModuleLoader: FC<DynamicModuleLoaderProps> = (props) => {
+  const { t } = useTranslation();
+  const {
+    children,
+    reducers,
+    removeAfterUnmount,
+  } = props;
+  const store = useStore() as ReduxStoreWithManager;
+  const dispatch = useDispatch();
+
+  // code splitting, то есть мы подгружаем редьюсер только в момент подгрузки компонента
+  // если компонент удаляется, редюсер тоже удаляем
+  useEffect(() => {
+    Object.entries(reducers).forEach(([name, reducer]: ReducersListEntry) => {
+      store.reducerManager.add(name, reducer);
+      dispatch({ type: `@INIT ${name} reducer` });
+    });
+
+    return () => {
+      if (removeAfterUnmount) {
+        Object.entries(reducers).forEach(([name, reducer]: ReducersListEntry) => {
+          store.reducerManager.remove(name);
+          dispatch({ type: `@DESTROY ${name} reducer` });
+        });
+      }
+    };
+    // eslint-disable-next-line
+  }, []);
+
+  return (
+    // eslint-disable-next-line react/jsx-no-useless-fragment
+    <>
+      {children}
+    </>
+  );
+};
